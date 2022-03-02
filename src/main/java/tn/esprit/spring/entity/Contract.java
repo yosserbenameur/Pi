eb8bb123b2a74fe119d9659d44cf76ec55d240b7
Ameni Latrach <Ameni.latrach@esprit.tn>
2022-03-02 11:44:20 +0100

    commit Ameni

--- src/main/java/tn/esprit/spring/entity/Contract.java
+++ src/main/java/tn/esprit/spring/entity/Contract.java
@@ -1,23 +1,23 @@
 package tn.esprit.spring.entity;
 
-import java.io.Serializable;
+//import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
 
-@Entity
-@Table(name="Contart")
-public class Contract implements Serializable {
+//@Entity
+//@Table(name="Contart")
+//public class Contract implements Serializable {
 
-	private static final long serialVersionUID= 1L;
-	@Id
-	@GeneratedValue( strategy = GenerationType.IDENTITY)
-	@Column (name= "ref_contrat")
-	private String ref_contrat;
+	//private static final long serialVersionUID= 1L;
+	//@Id
+	//@GeneratedValue( strategy = GenerationType.IDENTITY)
+	//@Column (name= "ref_contrat")
+	//private String ref_contrat;
 	
 	
-}
+//}
